0f37ccf85fd663acea43201d09a5249ccaaa20d4
Roman Polishchenko <romanpolishchenko@knu.ua>
2018-05-13 19:13:39 +0300

    Changed input

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from functions import *
 import datetime
 
 
+def input_data():
+    file_path = 'input/apps.csv'
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        _apps = read_csv(file)
+
+    file_path = 'input/link_data.csv'
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        _link_data = read_csv(file)
+
+    file_path = 'input/orders.csv'
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        _orders = read_csv(file)
+    return _apps, _link_data, _orders
+
+
 if __name__ == '__main__':
     CURRENT_TIME = datetime.datetime.utcnow().astimezone()
-
-    # file_path = 'input/apps.csv'
-    # with open(file_path, 'r', encoding='utf-8-sig') as file:
-    #     apps = read_csv(file)
-    #
-    # file_path = 'input/link_data.csv'
-    # with open(file_path, 'r', encoding='utf-8-sig') as file:
-    #     link_data = read_csv(file)
-    #
-    # file_path = 'input/orders.csv'
-    # with open(file_path, 'r', encoding='utf-8-sig') as file:
-    #     orders = read_csv(file)
-    #     # for i in orders:
-    #     #     print(i)
+    apps, link_data, orders = input_data()
